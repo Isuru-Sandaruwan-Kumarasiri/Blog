9615d9b0240cf41897641ee0f5f1fcf9018666a1
Isuru-Sandaruwan-Kumarasiri <s20426@sci.pdn.ac.lk>
2024-01-21 17:40:03 +0530

    created error page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 
 function ErrorPage() {
   return (
-    <div>ErrorPage</div>
+    <section className="error-page">
+      <div className="center">
+        <div><Link className="btn primary">Go Back Home</Link></div>
+        <h2>Page Not Found</h2>
+      </div>
+    </section>
   )
 }
 
