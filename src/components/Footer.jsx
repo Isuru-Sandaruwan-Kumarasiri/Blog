2c8b71c6f85a1fdc71124a284793424ad8730d4d
Isuru-Sandaruwan-Kumarasiri <s20426@sci.pdn.ac.lk>
2024-01-21 17:22:55 +0530

    created footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Link } from "react-router-dom";
+import "./footer.css"
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <footer>
+      <ul className="footer__categories">
+        <li><Link to="/posts/categories/python">Python</Link></li>
+        <li><Link to="/posts/categories/java">Java</Link></li>
+        <li><Link to="/posts/categories/javascript">Javascript</Link></li>
+        <li><Link to="/posts/categories/Nodejs">Nodejs</Link></li>
+      </ul>
+      <div className="footer__copyright">
+        <small>All Right Reserved &copy; copyright </small>
+      </div>
+    </footer>
   )
 }
 
