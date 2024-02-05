60172062bbb531ddfe30c37c2f7b3d9c02f84a7a
Isuru-Sandaruwan-Kumarasiri <s20426@sci.pdn.ac.lk>
2024-02-05 19:04:06 +0530

    created basic structure of createdPost page

--- src/pages/CategoryPosts.jsx
+++ src/pages/CategoryPosts.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function CategoryPosts() {
+  const[title,setTitle]=useState('');
+  const[Category,setCategory]=useState("Uncategory");
+  const[description,setDescription]=useState('');
+  const[img,setImg]=useState('');
+
   return (
-    <div>CategoryPosts</div>
+    <section className="create-post">
+      <div className="container">
+        <h2>Create Post</h2>
+        <p className="form__error-message">
+          This is an error message
+        </p>
+        <form action="" className="create-post__form">
+          <input type="text" placeholder='Title' value={title} />
+        </form>
+      </div>
+    </section>
   )
 }
 
